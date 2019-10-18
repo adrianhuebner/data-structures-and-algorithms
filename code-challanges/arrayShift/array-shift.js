f91c2a673c44234b190d05e90cd4658a569ded81
adrianhuebner <adrianh92@gmail.com>
2019-10-17 22:25:04 -0700

    started on code for code challenge

---
+++ code-challanges/arrayShift/array-shift.js
@@ -0,0 +1,18 @@
+// Pseudu Code (added on after whiteboarding)
+// create a function that takes in an array and a new value
+// create array variable and new value variable
+// create a for loop that will iterate through that array until it reaches the midpoint
+// insert new value into array at the midpoint
+
+const insertShiftArray (arr, val){
+  let midSpot = (arr.length - 1)/2;
+  // maybe need new array so: let newArr = [];
+  for(let i = 0; i >= (arr.length - 1)/2; i++){
+    if(arr[i] === midSpot);
+    // insert new Value into array
+  } else {
+    // keep going through array until midSpot is reached
+  }
+  return arr;
+}
+insertShiftArray(arr, val);
