fad460b03c7515d4952211b73db217b031e446c7
adrianhuebner <adrianh92@gmail.com>
2019-10-31 10:01:27 -0700

    code review added

--- code-challanges/multi-bracket-validation/multi-bracket-validation.js
+++ code-challanges/multi-bracket-validation/multi-bracket-validation.js
@@ -1,7 +1,24 @@
 'use strict';
 
 function multiBracketValidation(input){
-  let storage = [];
+  const bracketStr = input.split('').filter(char => '{}[]()'.includes(char)).join('');
+  const stack = [];
+  const map = {
+    ')' : '(',
+    ']' : '[',
+    '}' : '{'
+  }
+  for(let char of bracketStr){
+    if('{(['.includes(char)){
+      stack.push(char)
+    }
+    else{
+      let popped = stack.pop();
+      if(popped !== map[char]){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
-
-multiBracketValidation();
+multiBracketValidation('(){}[]codefellows[]');
