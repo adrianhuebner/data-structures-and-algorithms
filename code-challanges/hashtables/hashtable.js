46e46366b8c45d1cef29f54256d88c58e55d870d
adrianhuebner <adrianh92@gmail.com>
2019-12-08 19:58:53 -0800

    hashtables started

--- code-challanges/hashtables/hashtable.js
+++ code-challanges/hashtables/hashtable.js
@@ -1,11 +1,38 @@
-'use strict';
-
 class HashTable{
-  constructor(){
-    this.storage = new Array(100);
-    this.storageArray = []
+  constructor(size){
+    this.data = new Array(size);
   }
-  hash(string){
-    
-  }
+  _hash(key){
+    let hash = 0;
+    for(let i = 0; i < key.length; i++){
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+  return hash;
+  } // 0(1)
+
+  _add(key, value){
+    let address = this._hash(key)
+    if(!this.data[address]){
+      this.data[address] = [];
+    }
+      this.data[address].push([key, value]);
+      return this.data
+  } // 0(1)
+
+  _get(key){
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if(currentBucket.length){
+      for(let i = 0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key){
+          return currentBucket[i][1];
+        }
+      }
+    } else {
+      return undefined;
+    }
+  } // 0(1) if no collisions, 0(n)
 }
+
+const myHashTable = new HashTable(100);
+myHashTable._hash('grapes')
